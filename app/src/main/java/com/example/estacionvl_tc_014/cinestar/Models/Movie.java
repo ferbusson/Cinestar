4058be98e3c34f58d54fd29ca0cf1e1babb3d8ae
Andres Burbano <andres.after@gmail.com>
2016-02-02 19:35:27 -0500

    Added new gradient to the cinestar app

---
+++ app/src/main/java/com/example/estacionvl_tc_014/cinestar/Models/Movie.java
@@ -0,0 +1,44 @@
+package com.example.estacionvl_tc_014.cinestar.Models;
+
+/**
+ * Created by EstascionVL-TC-010 on 2/02/16.
+ */
+public class Movie {
+
+    String name;
+    float rating;
+    String lengthm;
+    String url;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public float getRating() {
+        return rating;
+    }
+
+    public void setRating(float rating) {
+        this.rating = rating;
+    }
+
+    public String getLengthm() {
+        return lengthm;
+    }
+
+    public void setLengthm(String lengthm) {
+        this.lengthm = lengthm;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+}
